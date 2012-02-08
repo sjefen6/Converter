97006865a58d86f19f2c13b639efb1d03c3c4e0d
Vegard Langås <sjefen6@gmail.com>
2012-02-08 18:37:30 +0100

    Made most of the strings, ui and the temperatureConverter (this is my first test for how to convert the values)

---
+++ src/org/goldclone/converter/temperatureConverter.java
@@ -0,0 +1,52 @@
+package org.goldclone.converter;
+
+public class temperatureConverter {
+	
+	//The standard temperature is Kelvin
+
+	private static temperatureConverter instance = null;
+	private double kelvin;
+
+	protected temperatureConverter() {
+		// Exists only to defeat instantiation.
+	}
+
+	public static temperatureConverter getInstance() {
+		if (instance == null) {
+			instance = new temperatureConverter();
+		}
+		return instance;
+	}
+	
+	public void setKelvin(double kelvin)
+	{
+		this.kelvin = kelvin;
+	}
+	
+	public void setCelsius(double celsius)
+	{
+		this.kelvin = celsius + 273.15;
+	}
+	
+	public void setFahrenheit(double fahrenheit)
+	{
+		this.kelvin = (fahrenheit + 459.67) * (5/9);
+	}
+	
+	public double getKelvin()
+	{
+		return this.kelvin;
+	}
+	
+	public double getCelsius()
+	{
+		return this.kelvin - 273.15;
+	}
+	
+	public double getFahrenheit()
+	{
+		return (this.kelvin * (9/5) - 459.67);
+	}
+	
+
+}
